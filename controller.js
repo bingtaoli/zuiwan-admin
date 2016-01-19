bf45b742701fb1052d89cb4ec774747d9896a3b0
bingtaoli <1464738780@qq.com>
2016-01-19 23:47:28 +0800

    use chart.js to draw picture

--- controller.js
+++ controller.js
@@ -18,10 +18,29 @@ var zuiwanControllers = angular.module('zuiwanControllers', []);
 
 zuiwanControllers.controller('VisitCtrl', ['$scope', function ($scope) {
 	function drawData(){
+		var data = {
+		    labels: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+		    datasets: [
+		        {
+		            label: "My Second dataset",
+		            fillColor: "rgba(151,187,205,0.2)",
+		            strokeColor: "rgba(151,187,205,1)",
+		            pointColor: "rgba(151,187,205,1)",
+		            pointStrokeColor: "#fff",
+		            pointHighlightFill: "#fff",
+		            pointHighlightStroke: "rgba(151,187,205,1)",
+		            data: [28, 48, 40, 19, 86, 27, 90]
+		        }
+		    ]
+		};
+		var width = $(".panel-body")[0].offsetWidth;
+		var canvas = document.getElementById("myChart");
+		canvas.width = width - 40;
+		var ctx = canvas.getContext("2d");
+		var myLineChart = new Chart(ctx).Line(data);
 	}
 	$scope.load = function(){
 		drawData();
-		console.log("drawData");
 	}
 }]);
 
