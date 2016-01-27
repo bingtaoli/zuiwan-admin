fb398fb78421b82669fffebf0942052b60db9e3b
bingtaoli <1464738780@qq.com>
2016-01-27 16:24:24 +0800

    数据统计添加热门文章功能,使用fushion代替chart.js

--- controller.js
+++ controller.js
@@ -16,32 +16,82 @@ function editor_init(){
 
 var zuiwanControllers = angular.module('zuiwanControllers', []);
 
-zuiwanControllers.controller('VisitCtrl', ['$scope', '$http', function ($scope, $http) {
-	function drawData(){
-		var data = {
-		    labels: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-		    datasets: [
-		        {
-		            label: "My Second dataset",
-		            fillColor: "rgba(151,187,205,0.2)",
-		            strokeColor: "rgba(151,187,205,1)",
-		            pointColor: "rgba(151,187,205,1)",
-		            pointStrokeColor: "#fff",
-		            pointHighlightFill: "#fff",
-		            pointHighlightStroke: "rgba(151,187,205,1)",
-		            data: [28, 48, 40, 19, 86, 27, 90]
-		        }
-		    ]
-		};
-		var width = $(".panel-body")[0].offsetWidth;
-		var canvas = document.getElementById("myChart");
-		canvas.width = width - 40;
-		var ctx = canvas.getContext("2d");
-		var myLineChart = new Chart(ctx).Line(data);
-	}
-	$scope.load = function(){
-		drawData();
+zuiwanControllers.controller('VisitCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+	$http({
+		method: "GET",
+		url: "http://localhost/zuiwan-backend/index.php/article/get_top_article",
+	}).success(function(data){
+		var topArticles = [];
+		var topArticleIds = [];
+		for (var i = 0; i < data.length; i++){
+			//限制文章标题最多字数
+			var article_title = data[i].article_title;
+			var maxLength = 13;
+			if (article_title.length > maxLength){
+				article_title = article_title.substr(0, maxLength) + "...";
+			}
+			topArticles.push({'label': article_title, 'value': data[i].visit_count});
+			topArticleIds.push(data[i].id);
+		}
+		$scope.topArticleIds = topArticleIds;
+		fushionDraw(topArticles);
+		//消除绘图库的水印
+		var spans = $('tspan');
+		for (var i = 0; i < spans.length; i++){
+			if ($(spans[i]).text() == "FusionCharts XT Trial"){
+				$(spans[i]).remove();
+				break;
+			}
+		}
+	});
+	function fushionDraw(topArticles){
+	    var revenueChart = new FusionCharts({
+	        "type": "bar2d",
+	        "renderAt": "chartContainer",
+	        "width": "600",
+	        "height": "300",
+	        "dataFormat": "json",
+	        "dataSource": {
+			    "chart": {
+			        "yAxisName": "访问量",
+			        //"numberPrefix": "$",
+			        "paletteColors": "#0075c2",
+			        "bgColor": "#ffffff",
+			        "showBorder": "0",
+			        "showCanvasBorder": "0",
+			        "usePlotGradientColor": "0",
+			        "plotBorderAlpha": "10",
+			        "placeValuesInside": "1",
+			        "valueFontColor": "#ffffff",
+			        "showAxisLines": "1",
+			        "axisLineAlpha": "25",
+			        "divLineAlpha": "10",
+			        "alignCaptionWithCanvas": "0",
+			        "showAlternateVGridColor": "0",
+			        "captionFontSize": "14",
+			        "subcaptionFontSize": "14",
+			        "subcaptionFontBold": "0",
+			        "toolTipColor": "#ffffff",
+			        "toolTipBorderThickness": "0",
+			        "toolTipBgColor": "#000000",
+			        "toolTipBgAlpha": "80",
+			        "toolTipBorderRadius": "2",
+			        "toolTipPadding": "5"
+			    },
+			    "data": topArticles
+			},
+			"events": {
+				"dataPlotClick": function(eventObj, dataObj) {
+                	var index = dataObj.index;
+                	log(index);
+                	var id = $scope.topArticleIds[index];
+                	$state.go('viewArticle', {id: id});
+            	}
+			}
+	    });
+	    revenueChart.render();
 	};
+
 	$http({
 		method: 'GET',
 		url: "http://115.28.75.190/zuiwan-backend/index.php/admin/get_website_information",
